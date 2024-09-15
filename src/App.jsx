bbfb0fc1028b907c00fc0e8120a8dca553f5b0e8
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-09-15 11:28:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+
+      <h1>Hi, I am Rifat</h1>
+      <p>Full Stack Developer From Bangladesh</p>
+
+
+    </>
+  )
+}
+
+export default App
