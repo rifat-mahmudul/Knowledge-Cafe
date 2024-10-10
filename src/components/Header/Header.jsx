9141ea2dcb0a47976a8dc088a6145063e9d18d78
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-10-10 19:23:49 +0600

    Navbar Done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
+import profile from '../../assets/images/profile.png'
+
 const Header = () => {
     return (
-        <div>
+        <div className='max-w-[1150px] mx-auto py-3 flex justify-between items-center border-b-2'>
         
-        <h1>Knowledge Cafe</h1>
+        <h1 className='font-extrabold text-3xl' >Knowledge Cafe</h1>
+        <img src={profile} alt="" />
 
         </div>
     )
